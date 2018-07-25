8d44aade3c2014cd3f8f17b98843110810200f3e
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-07-25 12:35:10 -0700

    format models

--- db/models/Song.js
+++ db/models/Song.js
@@ -2,20 +2,17 @@ const Sequelize = require('sequelize');
 const { db } = require('../index.js');
 
 const Song = db.define('song', {
-    songName: {
-        type: Sequelize.STRING
-    },
-    artist: {
-        type: Sequelize.STRING
-    },
-    genre: {
-        type: Sequelize.STRING
-    }
+  songName: {
+    type: Sequelize.STRING
+  },
+  artist: {
+    type: Sequelize.STRING
+  },
+  genre: {
+    type: Sequelize.STRING
+  }
 });
 
-
 Song.sync();
-
-
 
 module.exports.Song = Song;
--- db/models/User.js
+++ db/models/User.js
@@ -1,15 +1,14 @@
 const Sequelize = require('sequelize');
 const { db } = require('../index.js');
-// console.log('sqlz' , db)
 
 const User = db.define('user', {
-    username: {
-        type: Sequelize.STRING,
-        unique: true
-    },
-    password: {
-        type: Sequelize.STRING
-    }
+  username: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING
+  }
 });
 
 User.sync();
