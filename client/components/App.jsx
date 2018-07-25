682d1f9924e30a773c54ee36c825820d5fa479d6
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-07-25 12:43:44 -0700

    annotation for the app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,44 +5,48 @@ import Search from './Search.jsx';
 import Results from './Results.jsx';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loggedIn: false,
-            currentUser: '',
-            currentSong: '',
-        };
-        this.login = this.login.bind(this);
-        this.searchResult = this.searchResult.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      currentUser: '',
+      currentSong: '',
     };
+    this.login = this.login.bind(this);
+    this.searchResult = this.searchResult.bind(this);
+  };
 
-    searchResult(results) {
-        console.log(' what we get back', results)
-        this.setState({
-            currentSong: results.data.value
-        })
-    }
+  searchResult(results) {
+    console.log(' what we get back', results)
+    this.setState({
+      currentSong: results.data.value
+    })
+  }
 
-    login() {
-        console.log('this works')
-        this.setState({
-            loggedIn: true, 
-        });
-        this.setState(this.State);
-        // console.log('state', this.state);
-    };
-    //create a function that would change loggedIn state to true or false
-        //passed in that function to login as props with binding
-    
+  // ==== temporary login fix before utilizing Oauth ==== 
 
-    render() {
-        return (
-            <div>
-                {this.state.loggedIn ? <div>
-                <Search sendBack={this.searchResult} />
-                <Results quote={this.state.currentSong} />
-                </div> : <Login onLogin={this.login} /> }
-            </div>
-        )
-    };  
+  login() {
+    console.log('this works')
+    this.setState({
+      loggedIn: true, 
+    });
+    this.setState(this.State);
+  };
+
+  //create a function that would change loggedIn state to true or false
+  //passed in that function to login as props with binding
+
+
+// ==== temporary conditional rendering, implement next.js soon ====
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? <div>
+        <Search sendBack={this.searchResult} />
+        <Results quote={this.state.currentSong} />
+        </div> : <Login onLogin={this.login} /> }
+      </div>
+    )
+  };  
 };
