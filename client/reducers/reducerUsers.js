d735bfaf1a9d6d96aa0a2dfb4a7b772191d37f4f
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-07-25 18:52:05 -0700

    added react-redux dependency

---
+++ client/reducers/reducerUsers.js
@@ -0,0 +1,13 @@
+
+export default function() {
+  return [
+    {
+      username: 'Arthur',
+      password: '12345'
+    },
+    {
+      username: 'Billy',
+      password: 'Tables'
+    }   
+  ]
+}
