758a420efbf54d3c38a84fe1a960ceff05121796
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2020-07-14 17:21:49 -0700

    [upgrade] packages to current versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const SRC_DIR = path.resolve(__dirname, 'client');
-const BUILD_DIR = path.resolve(__dirname, 'static');
+const SRC_DIR = path.resolve(__dirname, "client");
+const BUILD_DIR = path.resolve(__dirname, "static");
 
 module.exports = {
-  entry: path.resolve(SRC_DIR, 'index.jsx'),
+  entry: path.resolve(SRC_DIR, "index.jsx"),
   output: {
-    filename: 'bundle.js',
-    path: BUILD_DIR
+    filename: "bundle.js",
+    path: BUILD_DIR,
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015', 'react'] }
-        }],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
-    ]
-  }
-}
+    ],
+  },
+};
